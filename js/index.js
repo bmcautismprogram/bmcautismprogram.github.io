7547f7fa3313333c30f172821b754ca3e2f01b68
Sharon Chao <sharonchao1@gmail.com>
2019-01-16 11:37:03 -0500

    add shapiro dermatology

--- js/index.js
+++ js/index.js
@@ -120,7 +120,7 @@ function configureDropDownLists(building,department) {
     var doctorsoffice = ['Psychiatry Research Center'];
     var dowling = ['Behavioral Health', 'Child Psychiatry'];
     var preston = ['Blood Draw Station','Cardiovascular Center', 'Center for Endocrinology, Diabetes, Nutrition & Weight Management', 'Renal Medicine', 'TB Clinic'];
-    var shapiro = ['Adult Primary Care', 'Blood Collection Station', 'Neurology', 'Pediatric Neurology'];
+    var shapiro = ['Adult Primary Care', 'Blood Collection Station', 'Dermatology', 'Neurology', 'Pediatric Neurology'];
     var yawkey = ['Adolescent Medicine', 'Blood Draw Station', 'Dentistry', 'Family Medicine', 'Ophthalmology', 'Pediatric Primary Care', 'Pediatric Dentistry', 'Pediatric Specialty'];
 
     switch (building.value) {
